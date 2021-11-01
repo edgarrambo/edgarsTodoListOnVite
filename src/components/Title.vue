3e51cbd7e6d463c665b63ec47890ef2d957f296e
Edgar Alonzo <ragde956@gmail.com>
2021-10-31 22:46:36 -0500

    Scaffold Vue Todoapp

---
+++ src/components/Title.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+const newTodo = ref("");
+import useTodos from "../composables/useTodos";
+const {addTodo} = useTodos();
+
+
+
+const add = () => {
+    if(newTodo.value) {
+        addTodo(newTodo.value);
+        newTodo.value = "";
+    }
+};
+</script>
+
+<template>
+<hi class="text-6xl font-thin tracking-tight text-center">🥸 Edgar's Weekend Todo List App 🥸</hi>
+
+<input 
+@change="add"
+v-model="newTodo"
+type="text" class="px-4 py-2 text-xl text-center rounded-lg" 
+placeholder="New Task(Please no more)" />
+</template>
